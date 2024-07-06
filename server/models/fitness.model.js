4fa665ee071f1aeb549ff21eb1ec20c637b99509
Kalyn Huff <huffkalyn@yahoo.com>
2024-07-06 11:57:43 -0400

    initial commit

---
+++ server/models/fitness.model.js
@@ -0,0 +1,36 @@
+import { Schema, model } from "mongoose";
+
+const fitnessSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required."],
+      minLength: [2, "Name should be at least two characters."],
+      maxLength: [50, "Name can be not longer than fifty characters."],
+    },
+    title: {
+      type: String,
+      required: [true, " Title is required"],
+      minLength: [5, "Title should be at least Five characters"],
+      maxLength: [50, "Title cannot be more than fifty characters"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      minLength: [2, "Description should be at least two characters"],
+      maxLength: [50, "Title cannot be more than fifty characters"],
+    },
+    date: {
+      type: Date,
+      required: [true],
+    },
+    length_of_time: {
+      type: Number,
+      required: [true],
+      min: [5, "Come on, you can do more than a five minute workout!!"],
+    },
+  },
+  { timestamps: true }
+);
+const Fitness = model("fitness", fitnessSchema);
+export default Fitness;
